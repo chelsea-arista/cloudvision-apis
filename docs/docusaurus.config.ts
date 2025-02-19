963cea32f0af34b8063474dc7d4b2903e22ed8ed
Chelsea Liu <chelsea@arista.com>
2025-02-19 12:59:39 -0600

    trunk

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -40,6 +40,12 @@ const config: Config = {
           editUrl:
             'https://github.com/aristanetworks/cloudvision-apis/edit/main/docs/',
           routeBasePath: 'docs',
+          lastVersion: 'current',
+          versions: {
+            current: {
+              label: 'Trunk',
+            },
+          },
         },
         blog: false,
         theme: {
